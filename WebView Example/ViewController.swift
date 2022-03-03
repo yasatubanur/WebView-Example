84b91694f5c8d92d9a0d8adb791992ba9a98240c
Tuba Nur <yasatubanur@gmail.com>
2022-03-03 23:17:01 +0300

    url name refactored.

--- WebView Example/ViewController.swift
+++ WebView Example/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView : WKWebView!
     var progressView : UIProgressView!
+    var websites = ["apple.com","hackingwithswift.com","www.youtube.com/watch?v=nLmtfI1UtXQ&t=634s"]
     
     override func loadView() {
         webView = WKWebView()
@@ -35,16 +36,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-        let url = URL(string: "https://www.apple.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
     @objc func openTapped(){
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "www.youtube.com/watch?v=nLmtfI1UtXQ&t=634s", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac,animated: true)
@@ -64,6 +65,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
         if keyPath == "estimatedProgress"{
             progressView.progress = Float(webView.estimatedProgress)
         }
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host{
+            for website in websites {
+                if host.contains(website){
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
     }
 }
 
